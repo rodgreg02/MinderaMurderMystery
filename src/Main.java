bf4716834e82532e4f3387d155a0355c0509f561
PedroBarge <pedro525tavares@gmail.com>
2023-12-06 13:11:19 +0000

    updsate main
    delete repeat code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-public class Main {
-    public static void main(String[] args) {
-
-=======
 import Characters.Player;
 import GameCore.Render;
 import Rooms.Room;
@@ -13,6 +7,5 @@ public class Main {
     public static void main(String[] args) {
         Room[] map = Room.createMap();
         Render.drawMap(map);
->>>>>>> 29eb30a8379e0a3efa7776c1301c2640244d8dd6
     }
 }
