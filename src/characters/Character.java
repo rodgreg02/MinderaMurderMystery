ff2bb3377729001fd0db56bc145e74edfcfca070
rodgreg02 <mindera@ME00391.local>
2023-12-07 16:15:58 +0000

    fixed name of class

--- src/characters/Character.java
+++ src/characters/Character.java
@@ -38,21 +38,21 @@ public class Character extends Card {
     public static void createCharacters() {
 
 
-        Characters Bruna = new Characters("Bruna", "Thorough observation revealed suspicious stains. Blood in the photo? This clue is macabre, but essential to the investigation.",
+        Character Bruna = new Character("Bruna", "Thorough observation revealed suspicious stains. Blood in the photo? This clue is macabre, but essential to the investigation.",
                 "Did you find blood in the photo? I bet it's just a lighting trick or something.", false);
-        Characters Francisco = new Characters("Francisco",
+        Character Francisco = new Character("Francisco",
                 "In this photo, one piece of information turned out to be crucial. We've discovered a clue that takes us straight to the heart of the mystery!",
                 "Clues in a photo? This feels more like a movie than a real investigation.", false);
-        Characters Fatima = new Characters("Fátima",
+        Character Fatima = new Character("Fátima",
                 "The date on the edge of the photo is the key to connecting the events. This clue is a missing link we have found!" ,
                 "Data and time in the image? This can be easily manipulated. How can we trust this?", false);
-        Characters Paulo = new Characters("Paulo",
+        Character Paulo = new Character("Paulo",
                 "The location in the photograph is our next point of interest. This geographic clue will guide us in the search for the truth.",
                 "Data, locations and times can be manipulated. I need more solid evidence than that.", false);
-        Characters David = new Characters("David",
+        Character David = new Character("David",
                 "When examining the exact time of the photo, we noticed something extraordinary. This clue is like a clock that ticks the mystery revealer!",
                 "This 'clue' seems more like a creative interpretation than a solid discovery.", false);
-        Characters Rodrigo = new Characters("Edgar",
+        Character Rodrigo = new Character("Edgar",
                 "The location in the photo corresponds to a specific place. We're closer to solving the puzzle thanks to this clue!",
                 "Specific location in the photo? Or someone was simply traveling and decided to take a photo.", false);
 
