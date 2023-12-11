3bedc56f9bf9bbf72c06ebb9b2331a88bd77ec1f
Tiago <mindera@ME00565.local>
2023-12-11 10:48:35 +0000

    Add desc of clue object in player bag

--- src/characters/Player.java
+++ src/characters/Player.java
@@ -48,7 +48,7 @@ public class Player {
         System.out.println(".");
         Thread.sleep(1000);
         System.out.println(room.getClueObject());
-        detectiveBag.add(room.getClues().getName());
+        detectiveBag.add(room.getClues().getName()+ " - " +room.getClueObject());
     }
 
     public void checkInventory(){
