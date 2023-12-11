840f4e64bc0d34a3b49524467ce8da0463f42e15
PedroBarge <pedro525tavares@gmail.com>
2023-12-11 16:30:22 +0000

    Characters ending

---
+++ src/characters/CharactersEnding.java
@@ -0,0 +1,14 @@
+package characters;
+
+import room.Room;
+import weapon.Weapon;
+
+public class CharactersEnding {
+    public void charactersEnding(){
+        System.out.println(Character.suspects.get(3).getName());
+        System.out.println(Weapon.arrayListWeapons.get(3).getName());
+        System.out.println(Room.rooms.get(3).getName());
+
+
+    }
+}
