20070352c42bb8df5b0ed0a9d036112a29f9a231
PedroBarge <pedro525tavares@gmail.com>
2023-12-12 10:06:53 +0000

    CharactersEnding done

--- src/characters/CharactersEnding.java
+++ src/characters/CharactersEnding.java
@@ -11,12 +11,18 @@ public class CharactersEnding {
         System.out.print("It was during the night that ");
         int counter = 0;
         boolean seeIfCounterMax = false;
+
         for (int i = 0; i < 6; i++) {
             if (Character.suspects.get(i).getWasUsedToKill()) {
-                System.out.print(Character.suspects.get(i).getName() + " and used a ");
+                System.out.print(Character.suspects.get(i).getName() + " used a ");
             }
             if (Weapon.arrayListWeapons.get(i).getWasUsedToKill()) {
-                System.out.print(Weapon.arrayListWeapons.get(i).getName() + " to kill Flávio in that room");
+                System.out.print(Weapon.arrayListWeapons.get(i).getName() + " to kill Flávio in the ");
+            }
+        }
+        for (int i = 0; i < Room.rooms.size(); i++) {
+            if(Room.rooms.get(i).getWasUsedToKill()){
+                System.out.print(Room.rooms.get(i).getName());
             }
         }
         System.out.println();
--- src/gamecore/Game.java
+++ src/gamecore/Game.java
@@ -1,6 +1,7 @@
 package gamecore;
 
 import characters.Character;
+import room.Room;
 import weapon.Weapon;
 
 import java.util.ArrayList;
@@ -58,5 +59,24 @@ public class Game {
         }
         return selectedClues;
     }
+    public static void charactersEnding() {
+        System.out.print("It was during the night that ");
+        int counter = 0;
+        boolean seeIfCounterMax = false;
 
+        for (int i = 0; i < 6; i++) {
+            if (Character.suspects.get(i).getWasUsedToKill()) {
+                System.out.print(Character.suspects.get(i).getName() + " used a ");
+            }
+            if (Weapon.arrayListWeapons.get(i).getWasUsedToKill()) {
+                System.out.print(Weapon.arrayListWeapons.get(i).getName() + " to kill Flávio in the ");
+            }
+        }
+        for (int i = 0; i < Room.rooms.size(); i++) {
+            if(Room.rooms.get(i).getWasUsedToKill()){
+                System.out.print(Room.rooms.get(i).getName());
+            }
+        }
+        System.out.println();
+    }
 }
