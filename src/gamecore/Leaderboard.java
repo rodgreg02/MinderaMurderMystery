df2a0a6174033f8aaedca56caf16cca8b2a6109d
rodgreg02 <mindera@ME00391.local>
2023-12-22 13:46:50 +0000

    added position in leaderboard

--- src/gamecore/Leaderboard.java
+++ src/gamecore/Leaderboard.java
@@ -4,6 +4,7 @@ import file.FileManager;
 import file.WriteableFormat;
 
 import java.sql.Timestamp;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -31,11 +32,29 @@ public class Leaderboard {
         leaderboard.forEach(Leaderboard::printResultFormatted);
     }
 
+    public static void showLeaderBoard(){
+        Scanner scanner = new Scanner(System.in);
+        FileManager fm = new FileManager();
+        List<WriteableFormat> leaderboard = fm.readDatabase();
+        leaderboard = leaderboard.stream()
+                .sorted(Comparator.comparingLong(WriteableFormat::getTime))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < leaderboard.size(); i++) {
+            int position = i + 1;
+            WriteableFormat format = leaderboard.get(i);
+            System.out.print(position + " - ");
+            printResultFormatted(format);
+        }
+
+        System.out.println("Press any key to go to the main menu:");
+        scanner.next();
+    }
+
     private static void printResultFormatted(WriteableFormat writeableFormat) {
         long seconds = writeableFormat.getTime() / 1_000;
         long minutes = seconds / 60;
         seconds = seconds % 60;
         String formattedDuration = String.format("%02d:%02d - %s", minutes, seconds, writeableFormat.getName());
         System.out.println(formattedDuration);
-    }
-}
+    }}
